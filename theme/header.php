29bd2cb1afd6e33f94a0c2274c5e0e78570397ca
Paul Hayes <git@prhayes.com>
2013-06-13 23:09:23 +0100

    Increase subheading legibility
    * Increase font-size
    * Make headings bold
    * Bump CSS revision

--- theme/header.php
+++ theme/header.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script>(function(d){d.className=d.className.replace(/^no-js\b/,'js');}(document.documentElement));</script>
     <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-    <link rel="stylesheet" href="/css/master.css?14" />
+    <link rel="stylesheet" href="/css/master.css?15" />
     <?php wp_head(); ?>
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="author" href="https://plus.google.com/101305158833886436755?rel=author" />
